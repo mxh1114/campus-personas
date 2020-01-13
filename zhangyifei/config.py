c11046b24004fbbd4bfdce08ee217c65646f5323
zhangyf <zhangyf.dalian@gmail.com>
2020-01-13 11:24:52 +0800

    fasttext+xgboost模型baseline

--- zhangyifei/config.py
+++ zhangyifei/config.py
@@ -1,6 +1,13 @@
 # -*- coding:utf-8 -*-
 """
-@contact: adonis_wu@outlook.com
-@time: 2020-01-07
+@contact: zhangyf.dalian@gmail.com
+@time: 2020-01-09
+
 """
-__author__ = 'adowu'
+__author__ = 'zhangyf'
+
+# 原始数据train.csv所在路径
+data_dir = r'F:\sougou_personas1'
+
+# 模型存哪里
+model_dir = r'F:\sougou_personas1\models'
